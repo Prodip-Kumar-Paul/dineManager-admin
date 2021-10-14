66bc6a660a5a060047c34adfd2f0a6ac7c23ca26
Bijoy Das <dasbijoy9804417767@gmail.com>
2021-10-14 22:52:57 +0530

    Added form control

--- src/pages/signin/Signin.tsx
+++ src/pages/signin/Signin.tsx
@@ -1,37 +1,87 @@
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React, { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import signIn from "../../services/api/signin";
 
 function Copyright(props: any) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="#">
         dinemanager.com
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const theme = createTheme();
 
+interface Credentials {
+  email: {
+    value: string;
+    hasError: boolean;
+  };
+  password: {
+    value: string;
+    hasError: boolean;
+  };
+}
+
 export default function SignIn() {
+  const [email, setEmail] = useState<String>("");
+  const [password, setPasword] = useState<String>("");
+  const [hasError, setHasError] = useState<Boolean>(false);
+  const [credentials, setCredentials] = useState<Credentials>({
+    email: {
+      value: "",
+      hasError: false,
+    },
+    password: {
+      value: "",
+      hasError: false,
+    },
+  });
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const field = event.currentTarget.id;
+    const value = event.currentTarget.value;
+    if (field === "email") {
+      setCredentials({
+        ...credentials,
+        email: {
+          value,
+          hasError:
+            value === "" || !value.includes("@") || !value.includes(".")
+              ? true
+              : false,
+        },
+      });
+    } else if (field === "password") {
+      setCredentials({
+        ...credentials,
+        password: { value, hasError: value === "" ? true : false },
+      });
+    }
+  };
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    signIn(credentials.email.value, credentials.password.value);
   };
 
   return (
@@ -41,17 +91,21 @@ export default function SignIn() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          </Avatar>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
           <Typography component="h1" variant="h5">
-            Sign In 
+            Sign In
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
@@ -59,8 +113,13 @@ export default function SignIn() {
               id="email"
               label="Email Address"
               name="email"
-              autoComplete="email"
-              autoFocus
+              onChange={changeHandler}
+              error={credentials.email.hasError}
+              helperText={
+                credentials.email.hasError
+                  ? "Please enter a valid email address"
+                  : ""
+              }
             />
             <TextField
               margin="normal"
@@ -70,7 +129,13 @@ export default function SignIn() {
               label="Password"
               type="password"
               id="password"
-              autoComplete="current-password"
+              onChange={changeHandler}
+              error={credentials.password.hasError}
+              helperText={
+                credentials.password.hasError
+                  ? "Please enter a valid password"
+                  : ""
+              }
             />
             <Button
               type="submit"
--- src/services/api/signin.ts
+++ src/services/api/signin.ts
@@ -1,1 +1,21 @@
-export {}
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+  
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+import {getAuth, signInWithEmailAndPassword} from "firebase/auth"
+
+const auth = getAuth();
+
+const signIn = (email: string, password: string) => {
+    signInWithEmailAndPassword(auth, email, password).then(res => {
+        console.log(res);
+    }).catch(err => {
+        console.log(err);
+    })
+}
+
+export default signIn
