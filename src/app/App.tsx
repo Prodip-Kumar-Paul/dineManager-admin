25fe5b844a2d08ec0e6c008ef8159f203004dfa6
Prodip-Kumar-Paul <prodipkumarpaul708@gmail.com>
2021-10-17 22:38:45 +0530

    add restaurant list

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,26 @@
-import {Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 import Signin from "../pages/signin/Signin";
+import RestaurantList from "../pages/restaurant-details/RestaturantList";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Switch>
-        <Route path="/app">
-          <h1>Admin section</h1>
-        </Route>
-        <Route path="/">
-        <Signin />
-        </Route>
-      </Switch>
-    </div>
+      <div className="App">
+        <Switch>
+          <Route path="/app">
+            <h1>Admin section</h1>
+          </Route>
+          <Route path="/list">
+            <RestaurantList />
+          </Route>
+          <Route path="/">
+            <Signin />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
